3192d818ece3376495405693536eeccbbe74ba8d
ChristopheMalo <christof.malo@gmail.com>
2016-03-07 18:09:35 +0100

    style: delete space

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,7 @@
  */
 
 // Home page
-$app->get('/', function () use ($app) {
+$app->get('/', function() use ($app) {
     $links = $app['dao.link']->findAll();
     return $app['twig']->render('index.html.twig', array('links' => $links));
 })->bind('home');
