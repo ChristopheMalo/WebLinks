4e894077b466704f60f69c49abe0b7dcf5158937
ChristopheMalo <christof.malo@gmail.com>
2016-03-07 16:57:49 +0100

    docs: comment routes file

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Controller management of application routes
+ * 
+ * @author      Christophe Malo
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Baptiste Pesquet
+ */
 
 // Home page
 $app->get('/', function () use ($app) {
