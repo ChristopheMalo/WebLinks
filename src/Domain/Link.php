bfa8ab70ad7099188445c2cf251ea17f3b744f1d
ChristopheMalo <christof.malo@gmail.com>
2016-03-07 18:47:28 +0100

    refactor: reorder ands comments methods

--- src/Domain/Link.php
+++ src/Domain/Link.php
@@ -2,49 +2,96 @@
 
 namespace WebLinks\Domain;
 
+/**
+ * Class representing a Link
+ *
+ * @author      Christophe Malo
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Baptiste Pesquet
+ */
+
 class Link 
 {
     /**
-     * Link id.
+     * Link id
      *
      * @var integer
      */
     private $id;
 
     /**
-     * Link title.
+     * Link title
      *
      * @var string
      */
     private $title;
 
     /**
-     * Link url.
+     * Link url
      *
      * @var string
      */
     private $url;
 
+    
+    
+    
+    /**
+     * Returns link id
+     * 
+     * @return integer $id The link id
+     */
     public function getId() {
         return $this->id;
     }
 
-    public function setId($id) {
-        $this->id = $id;
-    }
-
+    /**
+     * Returns link title
+     * 
+     * @return string $title The link title
+     */
     public function getTitle() {
         return $this->title;
     }
-
-    public function setTitle($title) {
-        $this->title = $title;
-    }
-
+    
+    /**
+     * Returns link url
+     * 
+     * @return string $url The link url
+     */
     public function getUrl() {
         return $this->url;
     }
 
+    
+    
+    
+    /**
+     * Sets link id
+     * 
+     * @param int $id
+     * @return void
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    /**
+     * Sets link title
+     * 
+     * @param string $title
+     * @return void
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    /**
+     * Sets link url
+     * 
+     * @param string $url
+     * @return void
+     */
     public function setUrl($url) {
         $this->url = $url;
     }
