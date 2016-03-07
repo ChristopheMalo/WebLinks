fdfce3a1f635cb07ae0c25c4ba5250fe4138141f
ChristopheMalo <christof.malo@gmail.com>
2016-03-07 16:49:18 +0100

    docs: comment app file

--- app/app.php
+++ app/app.php
@@ -3,13 +3,21 @@
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
 
+/**
+ * Configuration file of Silex Application
+ * 
+ * @author      Christophe Malo
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Baptiste Pesquet
+ */
+
 // Register global error and exception handlers
 ErrorHandler::register();
 ExceptionHandler::register();
 
 // Register service providers
-$app->register(new Silex\Provider\DoctrineServiceProvider());
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app->register(new Silex\Provider\DoctrineServiceProvider());       // Doctrine DBAL
+$app->register(new Silex\Provider\TwigServiceProvider(), array(     // Twig
     'twig.path' => __DIR__.'/../views',
 ));
 
