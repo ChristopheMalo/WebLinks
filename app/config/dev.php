9fa588e85e98ee4028d2c166e7240453a3405efe
ChristopheMalo <christof.malo@gmail.com>
2016-03-08 12:18:57 +0100

    feat: code monolog level and update dev file

--- app/config/dev.php
+++ app/config/dev.php
@@ -7,8 +7,19 @@
  * @copyright   OpenClassrooms - Baptiste Pesquet
  */
 
-// include the prod configuration
-require __DIR__.'/prod.php';
+// Doctrine DBAL (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => '127.0.0.1',
+    'port'     => '3306',
+    'dbname'   => 'weblinks',
+    'user'     => 'weblinks_user',
+    'password' => 'secret',
+);
 
 // enable the debug mode
 $app['debug'] = true;
+
+// Sets the level of logs
+$app['monolog.level'] = 'INFO';
--- app/config/prod.php
+++ app/config/prod.php
@@ -1,19 +1,22 @@
 <?php
 /**
- * Connection configuration file to the DB via Doctrine DBAL
+ * Configuration file for the production phase
  * 
  * @author      Christophe Malo
  * @version     1.0.0
  * @copyright   OpenClassrooms - Baptiste Pesquet
  */
 
-// Doctrine (db)
+// Doctrine DBAL (db)
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => '127.0.0.1',
+    'host'     => 'localhost',
     'port'     => '3306',
     'dbname'   => 'weblinks',
     'user'     => 'weblinks_user',
     'password' => 'secret',
 );
+
+// Sets the level of logs
+$app['monolog.level'] = 'WARNING';
