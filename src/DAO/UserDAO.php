c433edbbd046c8b9dad4296940dd0f7307738870
ChristopheMalo <christof.malo@gmail.com>
2016-03-08 16:15:07 +0100

    feat: code all user methods

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -2,6 +2,10 @@
 
 namespace WebLinks\DAO;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use WebLinks\Domain\User;
 
 /**
@@ -12,7 +16,7 @@ use WebLinks\Domain\User;
  * @copyright   OpenClassrooms - Baptiste Pesquet
  */
 
-class UserDAO extends DAO
+class UserDAO extends DAO implements UserProviderInterface
 {
     /**
      * Returns a User matching the param id
@@ -60,6 +64,85 @@ class UserDAO extends DAO
     }
     
     /**
+     * Saves the user in database
+     * 
+     * @param \WebLinks\Domain\User $user The user to save
+     */
+    public function save(User $user)
+    {
+        // Buils array with user values
+        $userData = array(
+            'user_name'     => $user->getUsername(),
+            'user_salt'     => $user->getSalt(),
+            'user_password' => $user->getPassword(),
+            'user_role'     => $user->getRole()
+        );
+        
+        if ($user->getId())
+        {
+            // The user exists: update it
+            $this->getDb()->update('t_user', $userData, array('user_id' => $user->getId()));
+        }
+        else
+        {
+            // The user does not exist: insert it
+            $this->getDb()->insert('t_user', $userData);
+            
+            // Gets the id of the newly created user and sets it in the entity
+            $id = $this->getDb()->lastInsertId();
+            $user->setId($id);
+        }
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function loadUserByUsername($username) {
+        $sql = "select * from t_user where user_name=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($username));
+
+        if ($row)
+        {
+            return $this->buildDomainObject($row);
+        }
+        else
+        {
+            throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+        }
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function refreshUser(UserInterface $user) {
+        $class = get_class($user);
+        if (!$this->supportsClass($class))
+        {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $class));
+        }
+        return $this->loadUserByUsername($user->getUsername());
+
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    public function supportsClass($class) {
+        return 'WebLinks\Domain\User' === $class;
+    }
+    
+    /**
+     * Removes an user from the database
+     * 
+     * @param int $id The user id
+     */
+    public function delete($id)
+    {
+        // Efface l'utilisateur
+        $this->getDb()->delete('t_user', array('user_id' => $id));
+    }
+    
+    /**
      * Builds a User object based on a DB row
      * 
      * @param array $row The DB row containing User data
@@ -70,6 +153,9 @@ class UserDAO extends DAO
         $user = New User();
         $user->setId($row['user_id']);
         $user->setUsername($row['user_name']);
+        $user->setPassword($row['user_password']);
+        $user->setSalt($row['user_salt']);
+        $user->setRole($row['user_role']);
         
         return $user;
     }
