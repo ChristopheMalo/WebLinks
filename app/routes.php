c761f4c6dcd3c1f920f8ccece16fc15071f8a6c1
ChristopheMalo <christof.malo@gmail.com>
2016-03-08 10:46:20 +0100

    feat: update route for index action

--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,5 @@
  */
 
 // Home page
-$app->get('/', function() use ($app) {
-    $links = $app['dao.link']->findAll();
-    return $app['twig']->render('index.html.twig', array('links' => $links));
-})->bind('home');
+$app->get('/', "WebLinks\Controller\HomeController::indexAction")->bind('home');
+        
