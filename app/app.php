a2d04a18d7348d6948263ecf11ff871a682c5bbd
ChristopheMalo <christof.malo@gmail.com>
2016-03-08 12:14:34 +0100

    feat: register providers for cache, log and debug bar

--- app/app.php
+++ app/app.php
@@ -3,6 +3,7 @@
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
 
+
 /**
  * Configuration file of Silex Application
  * 
@@ -16,11 +17,24 @@ ErrorHandler::register();
 ExceptionHandler::register();
 
 // Register service providers
-$app->register(new Silex\Provider\DoctrineServiceProvider());       // Doctrine DBAL
-$app->register(new Silex\Provider\TwigServiceProvider(), array(     // Twig
+$app->register(new Silex\Provider\DoctrineServiceProvider());           // Doctrine DBAL
+$app->register(new Silex\Provider\TwigServiceProvider(), array(         // Twig
     'twig.path' => __DIR__.'/../views',
 ));
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());   // Twig-bridge - use PATH
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());       // Twig-bridge - use PATH
+$app->register(new Silex\Provider\MonologServiceProvider(), array(      // Log - Debug bar
+    'monolog.logfile' => __DIR__.'/../var/logs/weblinks.log',
+    'monolog.name' => 'WebLinks',
+    'monolog.level' => $app['monolog.level']
+));
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());  // Log- Debug bar
+if (isset($app['debug']) && $app['debug'])
+{
+    $app->register(new Silex\Provider\HttpFragmentServiceProvider());
+    $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
+        'profiler.cache_dir' => __DIR__.'/../var/cache/profiler'
+    ));
+}
 
 // Register services
 $app['dao.user'] = $app->share(function ($app)
